ba5c21186efdddd8cc6eb2750f59dbfee2c782d0
Emil Forslund <emil.forslund@pyknic.se>
2019-06-27 13:37:52 +0200

    Interpolate vertices

--- scripts/VoxelCell.gd
+++ scripts/VoxelCell.gd
@@ -3,13 +3,13 @@ extends MeshInstance
 
 var noise = OpenSimplexNoise.new()
 
-export var size = 10 setget setSize
-export var octaves = 4 setget setOctaves
-export var period = 20.0 setget setPeriod
+export var size        = 10 setget setSize
+export var octaves     = 4 setget setOctaves
+export var period      = 20.0 setget setPeriod
 export var persistence = 0.8 setget setPersistence
-export var surface = 0.0 setget setSurface
-export var noiseSeed = 0.0 setget setNoiseSeed
-export var cubeSize = 0.5
+export var surface     = 0.0 setget setSurface
+export var noiseSeed   = 0.0 setget setNoiseSeed
+export var cubeSize    = 0.5
 
 var vertices = PoolVector3Array()
 var normals  = PoolVector3Array()
@@ -56,7 +56,6 @@ func setNoiseSeed(val):
 	pass
 
 func density(at : Vector3):
-	#return at.length()
 	return noise.get_noise_3dv(translation + at) - at.y / 10
 
 func updateVoxel():
@@ -86,10 +85,6 @@ func updateVoxel():
 				for k in range(-size, size):
 					var pos = Vector3(i, j, k)
 					addVoxel(pos, cubeSize, minDensity, densityRange)
-					#var d = density(pos)
-					#if d <= surface:
-					#	#var gray = (d - minDensity) / densityRange
-					#	#addCube(pos, cubeSize, Color(gray, gray, gray))
 
 		var arrays = []
 		arrays.resize(ArrayMesh.ARRAY_MAX)
@@ -102,75 +97,73 @@ func updateVoxel():
 		self.mesh = arrayMesh
 	pass
 
-const CUBE_VERTICES = [
-	# front
-	Vector3(-1.0, -1.0, 1.0),
-	Vector3( 1.0, -1.0, 1.0),
-	Vector3( 1.0, 1.0, 1.0),
-	Vector3(-1.0, 1.0, 1.0),
-	
-	# right
-	Vector3(1.0, 1.0, 1.0),
-	Vector3(1.0, 1.0, -1.0),
-	Vector3(1.0, -1.0, -1.0),
-	Vector3(1.0, -1.0, 1.0),
-	
-	# back
-	Vector3(-1.0, -1.0, -1.0),
-	Vector3( 1.0, -1.0, -1.0),
-	Vector3( 1.0, 1.0, -1.0),
-	Vector3(-1.0, 1.0, -1.0),
-	
-	# left
-	Vector3(-1.0, -1.0, -1.0),
-	Vector3(-1.0, -1.0, 1.0),
-	Vector3(-1.0, 1.0, 1.0),
-	Vector3(-1.0, 1.0, -1.0),
-	
-	# upper
-	Vector3( 1.0, 1.0, 1.0),
-	Vector3(-1.0, 1.0, 1.0),
-	Vector3(-1.0, 1.0, -1.0),
-	Vector3( 1.0, 1.0, -1.0),
-	
-	# bottom
-	Vector3(-1.0, -1.0, -1.0),
-	Vector3( 1.0, -1.0, -1.0),
-	Vector3( 1.0, -1.0, 1.0),
-	Vector3(-1.0, -1.0, 1.0),
-]
-
-const CUBE_INDICES = [
-	 0,  2,  1,  0,  3,  2,   # front
-	 4,  5,  6,  4,  6,  7,   # right
-	 8,  9,  10, 8,  10, 11,  # back
-	 12, 14, 13, 12, 15, 14,  # left
-	 16, 17, 18, 16, 18, 19,  # upper
-	 20, 22, 21, 20, 23, 22   # bottom
-]
-
-func addCube(pos : Vector3, size : float, color : Color):
-	var offset = vertices.size()
-	
-	# Multiply each vertice with the size add add the position
-	for i in range(0, 6*4):
-		vertices.push_back(CUBE_VERTICES[i] * size + pos)
-		colors.push_back(color)
-		
-	for i in range(0, 6*6):
-		indices.push_back(offset + CUBE_INDICES[i])
-		
-	# The sum over all vertices on one side of the cube is equal to the 4 times the normal vector
-	for i in range(0, 6):
-		var n = Vector3(0, 0, 0)
-		for j in range(0, 4):
-			n += CUBE_VERTICES[i * 4 + j]
-		n /= 4.0
-		for j in range(0, 4):
-			normals.push_back(n)
-	pass
-
-
+#const CUBE_VERTICES = [
+#	# front
+#	Vector3(-1.0, -1.0, 1.0),
+#	Vector3( 1.0, -1.0, 1.0),
+#	Vector3( 1.0, 1.0, 1.0),
+#	Vector3(-1.0, 1.0, 1.0),
+#
+#	# right
+#	Vector3(1.0, 1.0, 1.0),
+#	Vector3(1.0, 1.0, -1.0),
+#	Vector3(1.0, -1.0, -1.0),
+#	Vector3(1.0, -1.0, 1.0),
+#
+#	# back
+#	Vector3(-1.0, -1.0, -1.0),
+#	Vector3( 1.0, -1.0, -1.0),
+#	Vector3( 1.0, 1.0, -1.0),
+#	Vector3(-1.0, 1.0, -1.0),
+#
+#	# left
+#	Vector3(-1.0, -1.0, -1.0),
+#	Vector3(-1.0, -1.0, 1.0),
+#	Vector3(-1.0, 1.0, 1.0),
+#	Vector3(-1.0, 1.0, -1.0),
+#
+#	# upper
+#	Vector3( 1.0, 1.0, 1.0),
+#	Vector3(-1.0, 1.0, 1.0),
+#	Vector3(-1.0, 1.0, -1.0),
+#	Vector3( 1.0, 1.0, -1.0),
+#
+#	# bottom
+#	Vector3(-1.0, -1.0, -1.0),
+#	Vector3( 1.0, -1.0, -1.0),
+#	Vector3( 1.0, -1.0, 1.0),
+#	Vector3(-1.0, -1.0, 1.0),
+#]
+#
+#const CUBE_INDICES = [
+#	 0,  2,  1,  0,  3,  2,   # front
+#	 4,  5,  6,  4,  6,  7,   # right
+#	 8,  9,  10, 8,  10, 11,  # back
+#	 12, 14, 13, 12, 15, 14,  # left
+#	 16, 17, 18, 16, 18, 19,  # upper
+#	 20, 22, 21, 20, 23, 22   # bottom
+#]
+#
+#func addCube(pos : Vector3, size : float, color : Color):
+#	var offset = vertices.size()
+#
+#	# Multiply each vertice with the size add add the position
+#	for i in range(0, 6*4):
+#		vertices.push_back(CUBE_VERTICES[i] * size + pos)
+#		colors.push_back(color)
+#
+#	for i in range(0, 6*6):
+#		indices.push_back(offset + CUBE_INDICES[i])
+#
+#	# The sum over all vertices on one side of the cube is equal to the 4 times the normal vector
+#	for i in range(0, 6):
+#		var n = Vector3(0, 0, 0)
+#		for j in range(0, 4):
+#			n += CUBE_VERTICES[i * 4 + j]
+#		n /= 4.0
+#		for j in range(0, 4):
+#			normals.push_back(n)
+#	pass
 
 func addVoxel(pos : Vector3, size : float, densityMin : float, densityRange : float):
 	
@@ -181,31 +174,38 @@ func addVoxel(pos : Vector3, size : float, densityMin : float, densityRange : fl
 		if density(p) < surface:
 			cubeindex |= (0x1 << i)
 	
+	# Each row in the TRIANGLE_TABLE can consist of up to 5 triangles
 	for i in range(0, 5):
 		if TRIANGLE_TABLE[cubeindex][i * 3] >= 0:
-			var verticeSum = Vector3(0, 0, 0)
+			# Iterate over the corners of the triangle
 			for j in range(0, 3):
-				var edgeIdx = TRIANGLE_TABLE[cubeindex][i * 3 + j]
-				var cornerA = CUBE_CORNERS[CORNER_INDEX_A_FROM_EDGE[edgeIdx]]
-				var cornerB = CUBE_CORNERS[CORNER_INDEX_B_FROM_EDGE[edgeIdx]]
-				var cornerMean = (cornerA + cornerB) / 2
-				var vertice = pos + size * cornerMean
+				var edgeIdx : int = TRIANGLE_TABLE[cubeindex][i * 3 + j]
+				var cornerA : Vector3 = CUBE_CORNERS[CORNER_INDEX_A_FROM_EDGE[edgeIdx]]
+				var cornerB : Vector3 = CUBE_CORNERS[CORNER_INDEX_B_FROM_EDGE[edgeIdx]]
+				#var cornerMean = (cornerA + cornerB) / 2
 				
+				# Compute the braking point of the surface along the edge
+				var densityA = density(pos + size * cornerA)
+				var densityB = density(pos + size * cornerB)
+				var interpFactor = (surface - densityA) / (densityB - densityA)
+				var cornerIntrp = cornerA + (cornerB - cornerA) * interpFactor
+				var vertice = pos + size * cornerIntrp
+				
+				# If everything is working, then all visible triangles 
+				# should have roughly the same color
 				var gray = (density(vertice) - densityMin) / densityRange
 				colors.push_back(Color(gray, gray, gray))
 				
 				indices.push_back(vertices.size())
 				vertices.push_back(vertice)
-				verticeSum += cornerMean
 			
-			verticeSum /= 3
-			
+			# Compute the normal of the triangle as the cross product between two edges
 			var crossA : Vector3 = vertices[vertices.size() - 3]
 			var crossB : Vector3 = vertices[vertices.size() - 2]
 			var crossC : Vector3 = vertices[vertices.size() - 1]
-			var cross = (crossC - crossB).cross(crossC - crossA)
+			var normal : Vector3 = (crossC - crossB).cross(crossC - crossA).normalized()
 			for j in range(0, 3):
-				normals.push_back(cross)
+				normals.push_back(normal)
 	
 	pass
 	
@@ -224,41 +224,41 @@ const CUBE_CORNERS = [
 
 const CORNER_INDEX_A_FROM_EDGE = [0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3]
 const CORNER_INDEX_B_FROM_EDGE = [1, 2, 3, 0, 5, 6, 7, 4, 4, 5, 6, 7]
-
-const EDGE_TABLE = [
-	0x0  , 0x109, 0x203, 0x30a, 0x406, 0x50f, 0x605, 0x70c,
-	0x80c, 0x905, 0xa0f, 0xb06, 0xc0a, 0xd03, 0xe09, 0xf00,
-	0x190, 0x99 , 0x393, 0x29a, 0x596, 0x49f, 0x795, 0x69c,
-	0x99c, 0x895, 0xb9f, 0xa96, 0xd9a, 0xc93, 0xf99, 0xe90,
-	0x230, 0x339, 0x33 , 0x13a, 0x636, 0x73f, 0x435, 0x53c,
-	0xa3c, 0xb35, 0x83f, 0x936, 0xe3a, 0xf33, 0xc39, 0xd30,
-	0x3a0, 0x2a9, 0x1a3, 0xaa , 0x7a6, 0x6af, 0x5a5, 0x4ac,
-	0xbac, 0xaa5, 0x9af, 0x8a6, 0xfaa, 0xea3, 0xda9, 0xca0,
-	0x460, 0x569, 0x663, 0x76a, 0x66 , 0x16f, 0x265, 0x36c,
-	0xc6c, 0xd65, 0xe6f, 0xf66, 0x86a, 0x963, 0xa69, 0xb60,
-	0x5f0, 0x4f9, 0x7f3, 0x6fa, 0x1f6, 0xff , 0x3f5, 0x2fc,
-	0xdfc, 0xcf5, 0xfff, 0xef6, 0x9fa, 0x8f3, 0xbf9, 0xaf0,
-	0x650, 0x759, 0x453, 0x55a, 0x256, 0x35f, 0x55 , 0x15c,
-	0xe5c, 0xf55, 0xc5f, 0xd56, 0xa5a, 0xb53, 0x859, 0x950,
-	0x7c0, 0x6c9, 0x5c3, 0x4ca, 0x3c6, 0x2cf, 0x1c5, 0xcc ,
-	0xfcc, 0xec5, 0xdcf, 0xcc6, 0xbca, 0xac3, 0x9c9, 0x8c0,
-	0x8c0, 0x9c9, 0xac3, 0xbca, 0xcc6, 0xdcf, 0xec5, 0xfcc,
-	0xcc , 0x1c5, 0x2cf, 0x3c6, 0x4ca, 0x5c3, 0x6c9, 0x7c0,
-	0x950, 0x859, 0xb53, 0xa5a, 0xd56, 0xc5f, 0xf55, 0xe5c,
-	0x15c, 0x55 , 0x35f, 0x256, 0x55a, 0x453, 0x759, 0x650,
-	0xaf0, 0xbf9, 0x8f3, 0x9fa, 0xef6, 0xfff, 0xcf5, 0xdfc,
-	0x2fc, 0x3f5, 0xff , 0x1f6, 0x6fa, 0x7f3, 0x4f9, 0x5f0,
-	0xb60, 0xa69, 0x963, 0x86a, 0xf66, 0xe6f, 0xd65, 0xc6c,
-	0x36c, 0x265, 0x16f, 0x66 , 0x76a, 0x663, 0x569, 0x460,
-	0xca0, 0xda9, 0xea3, 0xfaa, 0x8a6, 0x9af, 0xaa5, 0xbac,
-	0x4ac, 0x5a5, 0x6af, 0x7a6, 0xaa , 0x1a3, 0x2a9, 0x3a0,
-	0xd30, 0xc39, 0xf33, 0xe3a, 0x936, 0x83f, 0xb35, 0xa3c,
-	0x53c, 0x435, 0x73f, 0x636, 0x13a, 0x33 , 0x339, 0x230,
-	0xe90, 0xf99, 0xc93, 0xd9a, 0xa96, 0xb9f, 0x895, 0x99c,
-	0x69c, 0x795, 0x49f, 0x596, 0x29a, 0x393, 0x99 , 0x190,
-	0xf00, 0xe09, 0xd03, 0xc0a, 0xb06, 0xa0f, 0x905, 0x80c,
-	0x70c, 0x605, 0x50f, 0x406, 0x30a, 0x203, 0x109, 0x0   
-];
+#
+#const EDGE_TABLE = [
+#	0x0  , 0x109, 0x203, 0x30a, 0x406, 0x50f, 0x605, 0x70c,
+#	0x80c, 0x905, 0xa0f, 0xb06, 0xc0a, 0xd03, 0xe09, 0xf00,
+#	0x190, 0x99 , 0x393, 0x29a, 0x596, 0x49f, 0x795, 0x69c,
+#	0x99c, 0x895, 0xb9f, 0xa96, 0xd9a, 0xc93, 0xf99, 0xe90,
+#	0x230, 0x339, 0x33 , 0x13a, 0x636, 0x73f, 0x435, 0x53c,
+#	0xa3c, 0xb35, 0x83f, 0x936, 0xe3a, 0xf33, 0xc39, 0xd30,
+#	0x3a0, 0x2a9, 0x1a3, 0xaa , 0x7a6, 0x6af, 0x5a5, 0x4ac,
+#	0xbac, 0xaa5, 0x9af, 0x8a6, 0xfaa, 0xea3, 0xda9, 0xca0,
+#	0x460, 0x569, 0x663, 0x76a, 0x66 , 0x16f, 0x265, 0x36c,
+#	0xc6c, 0xd65, 0xe6f, 0xf66, 0x86a, 0x963, 0xa69, 0xb60,
+#	0x5f0, 0x4f9, 0x7f3, 0x6fa, 0x1f6, 0xff , 0x3f5, 0x2fc,
+#	0xdfc, 0xcf5, 0xfff, 0xef6, 0x9fa, 0x8f3, 0xbf9, 0xaf0,
+#	0x650, 0x759, 0x453, 0x55a, 0x256, 0x35f, 0x55 , 0x15c,
+#	0xe5c, 0xf55, 0xc5f, 0xd56, 0xa5a, 0xb53, 0x859, 0x950,
+#	0x7c0, 0x6c9, 0x5c3, 0x4ca, 0x3c6, 0x2cf, 0x1c5, 0xcc ,
+#	0xfcc, 0xec5, 0xdcf, 0xcc6, 0xbca, 0xac3, 0x9c9, 0x8c0,
+#	0x8c0, 0x9c9, 0xac3, 0xbca, 0xcc6, 0xdcf, 0xec5, 0xfcc,
+#	0xcc , 0x1c5, 0x2cf, 0x3c6, 0x4ca, 0x5c3, 0x6c9, 0x7c0,
+#	0x950, 0x859, 0xb53, 0xa5a, 0xd56, 0xc5f, 0xf55, 0xe5c,
+#	0x15c, 0x55 , 0x35f, 0x256, 0x55a, 0x453, 0x759, 0x650,
+#	0xaf0, 0xbf9, 0x8f3, 0x9fa, 0xef6, 0xfff, 0xcf5, 0xdfc,
+#	0x2fc, 0x3f5, 0xff , 0x1f6, 0x6fa, 0x7f3, 0x4f9, 0x5f0,
+#	0xb60, 0xa69, 0x963, 0x86a, 0xf66, 0xe6f, 0xd65, 0xc6c,
+#	0x36c, 0x265, 0x16f, 0x66 , 0x76a, 0x663, 0x569, 0x460,
+#	0xca0, 0xda9, 0xea3, 0xfaa, 0x8a6, 0x9af, 0xaa5, 0xbac,
+#	0x4ac, 0x5a5, 0x6af, 0x7a6, 0xaa , 0x1a3, 0x2a9, 0x3a0,
+#	0xd30, 0xc39, 0xf33, 0xe3a, 0x936, 0x83f, 0xb35, 0xa3c,
+#	0x53c, 0x435, 0x73f, 0x636, 0x13a, 0x33 , 0x339, 0x230,
+#	0xe90, 0xf99, 0xc93, 0xd9a, 0xa96, 0xb9f, 0x895, 0x99c,
+#	0x69c, 0x795, 0x49f, 0x596, 0x29a, 0x393, 0x99 , 0x190,
+#	0xf00, 0xe09, 0xd03, 0xc0a, 0xb06, 0xa0f, 0x905, 0x80c,
+#	0x70c, 0x605, 0x50f, 0x406, 0x30a, 0x203, 0x109, 0x0   
+#];
 
 const TRIANGLE_TABLE = [
 	[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
